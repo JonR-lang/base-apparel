b1e5a551dd55c8f03257725f2d2f223534960edc
Johnny <110586185+JonR-lang@users.noreply.github.com>
2023-09-03 08:38:07 +0100

    This is my first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const submitForm = document.querySelector('#email-form');
+const input = document.querySelector('#e-mail')
+const warningMessage = document.querySelector('#warning');
+const errorIcon = document.querySelector('#icon-error')
+
+submitForm.addEventListener('submit', (e) => {
+    const email = input.value
+    if (!emailRegex.test(email)) {
+        warningMessage.textContent = "Please provide a valid email !"
+        errorIcon.style.display = 'block'
+        e.preventDefault()
+        input.style.borderColor = 'red'
+    }
+})
+
+function validateEmail(email) {
+    const emailRegex = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
+    return emailRegex.test(email)
+}
+
+console.log(validateEmail(sex))
